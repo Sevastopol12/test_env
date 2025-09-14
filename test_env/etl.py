2a4fab9fb1ebfa961ea4185ae220dad3584eaee0
Sevastopol12 <chkalov253@gmail.com>
2025-09-14 20:22:37 +0700

    chore: change runtime to 12pm everyday

--- test_env/etl.py
+++ test_env/etl.py
@@ -18,6 +18,9 @@ def load_comparison() -> pd.DataFrame:
     }
     comparison: pd.DataFrame = Screener(source="tcbs").stock(
         default_params, limit=1700, lang="en")
+    # Remove unstable columns
+    comparison = comparison.drop(
+        [x for x in comparison.columns if x.startswith("price_vs")], axis=1)
     return comparison
 
 
@@ -25,6 +28,27 @@ def load_price_board(tickers: list[str]) -> pd.DataFrame:
     price_board: pd.DataFrame = Trading(
         source='vci').price_board(symbols_list=tickers)
     price_board.columns = price_board.columns.droplevel(0)
+
+    # Compute instrument
+    if "match_price" in price_board.columns:
+        price_board = price_board.rename(
+            columns={"match_price": "current_price"})
+        price_board["price_change"] = price_board["current_price"] - \
+            price_board["ref_price"]
+        price_board["pct_price_change"] = (
+            price_board["price_change"] / price_board["ref_price"]) * 100
+
+    else:
+        price_board = price_board.rename(
+            columns={"ref_price": "current_price"})
+        price_board["price_change"] = 0
+        price_board["pct_price_change"] = 0
+
+    # Normalize
+    price_board["current_price"] = round(
+        price_board["current_price"] * 1e-3, 2)
+    price_board["price_change"] = round(price_board["price_change"] * 1e-3, 2)
+    price_board["pct_price_change"] = round(price_board["pct_price_change"], 2)
 
     return price_board
 
